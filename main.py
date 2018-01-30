ad52fbb0fd4a0dc19e7418bd71d98c3853299220
Ronpiche <vgspm@hotmail.fr>
2018-01-30 17:40:15 +0100

    work for the 1975 spell

--- main.py
+++ main.py
@@ -12,65 +12,70 @@ def fetchSpellHtmlFromDxcontent(spellNumber):
 
 
 def main():
-    maxSpellNumber = 10
+    maxSpellNumber = 1975 # max possible 1975
     spellDataTab = [];
     for i in range(1, maxSpellNumber+1):
-        spellHtml =fetchSpellHtmlFromDxcontent(i)
-        spellSoup = BeautifulSoup(spellHtml,"html.parser")
-        spellDiv = spellSoup.find("div", {"class": "SpellDiv"})
+        try:
+            spellHtml =fetchSpellHtmlFromDxcontent(i)
+            spellSoup = BeautifulSoup(spellHtml,"html.parser")
+            spellDiv = spellSoup.find("div", {"class": "SpellDiv"})
 
-        # Find if the spell is a wizard spell
-        spellSchoolList = str.split(spellDiv.findAll("p", {"class": "SPDet"})[0].text)
+            # Find if the spell is a wizard spell
+            spellSchoolList = str.split(spellDiv.findAll("p", {"class": "SPDet"})[0].text)
 
-        if ("sorcerer/wizard" in spellSchoolList) or ("wizard" in spellSchoolList):
-            # Find level of the spell
-            # take the next one in the list which is the level of the spell
-            if ("sorcerer/wizard" in spellSchoolList):
-                spellLevel = spellSchoolList[spellSchoolList.index("sorcerer/wizard") + 1]
-            else:
-                spellLevel = spellSchoolList[spellSchoolList.index("wizard") + 1]
-            # change the spellLevel string in int
-            spellLevel = int(spellLevel.strip(","))
+            if ("sorcerer/wizard" in spellSchoolList) or ("wizard" in spellSchoolList):
+                # Find level of the spell
+                # take the next one in the list which is the level of the spell
+                if ("sorcerer/wizard" in spellSchoolList):
+                    spellLevel = spellSchoolList[spellSchoolList.index("sorcerer/wizard") + 1]
+                else:
+                    spellLevel = spellSchoolList[spellSchoolList.index("wizard") + 1]
+                # change the spellLevel string in int
+                spellLevel = int(spellLevel.strip(","))
 
-            #Find the name of the spell
-            spellName = spellDiv.find("div", {"class": "heading"}).text
+                #Find the name of the spell
+                spellName = spellDiv.find("div", {"class": "heading"}).text
 
-            #Find spell component
-            spellComponentList_unrefined = str.split(spellDiv.findAll("p", {"class": "SPDet"})[2].text )
-            spellComponentList =[]
+                #Find spell component
+                spellComponentList_unrefined = str.split(spellDiv.findAll("p", {"class": "SPDet"})[2].text )
+                spellComponentList =[]
 
-            #refined the list
-            insideBraket =False
-            for y in range(1, len(spellComponentList_unrefined) ):#no need to test the first element
-                spellComponentList_unrefined[y] =spellComponentList_unrefined[y].strip(",")
-                if ( "(" in spellComponentList_unrefined[y] ):
-                    insideBraket = True
-                elif ( ")" in spellComponentList_unrefined[y] ):
-                    insideBraket = False
-                elif (not insideBraket):
-                    spellComponentList.append(spellComponentList_unrefined[y])
+                #refined the list
+                insideBraket =False
+                for y in range(1, len(spellComponentList_unrefined) ):#no need to test the first element
+                    spellComponentList_unrefined[y] =spellComponentList_unrefined[y].strip(",")
+                    if ( "(" in spellComponentList_unrefined[y] ):
+                        insideBraket = True
+                    elif ( ")" in spellComponentList_unrefined[y] ):
+                        insideBraket = False
+                    elif (not insideBraket):
+                        spellComponentList.append(spellComponentList_unrefined[y])
 
-            #find if there is a spell resistance
-            if ( len(spellDiv.findAll("p", {"class": "SPDet"}) ) == 6):
-                # old spell by default we put spell resistance at false
-                spellResistance =False;
-            elif ( len(spellDiv.findAll("p", {"class": "SPDet"}) ) == 7):
-                spellResistanceHtmlDiv = spellDiv.findAll("p", {"class": "SPDet"})[6];
-                if ("<b>Spell Resistance</b> no" in spellResistanceHtmlDiv):
-                    spellResistance =False
-                elif ("<b>Spell Resistance</b> yes" in spellResistanceHtmlDiv):
-                    spellResistance = True
-                else: #can be improve for particular case
-                    spellResistance =False; #value by default
+                #find if there is a spell resistance
+                if ( len(spellDiv.findAll("p", {"class": "SPDet"}) ) == 6):
+                    # old spell by default we put spell resistance at false
+                    spellResistance =False;
+                elif ( len(spellDiv.findAll("p", {"class": "SPDet"}) ) == 7):
+                    spellResistanceHtmlDiv = spellDiv.findAll("p", {"class": "SPDet"})[6];
+                    if ("<b>Spell Resistance</b> no" in spellResistanceHtmlDiv):
+                        spellResistance =False
+                    elif ("<b>Spell Resistance</b> yes" in spellResistanceHtmlDiv):
+                        spellResistance = True
+                    else: #can be improve for particular case
+                        spellResistance =False; #value by default
 
-            #create the spell data for this spell
-            spellData ={}
-            spellData["name"] = spellName
-            spellData["level"] = spellLevel
-            spellData["components"] = spellComponentList
-            spellData["spell_resistance"] = spellResistance
-            #put this data in a tab
-            spellDataTab.append(spellData)
+                #create the spell data for this spell
+                spellData ={}
+                spellData["name"] = spellName
+                spellData["level"] = spellLevel
+                spellData["components"] = spellComponentList
+                spellData["spell_resistance"] = spellResistance
+                #put this data in a tab
+                spellDataTab.append(spellData)
+                #print(i)
+        except (AttributeError):
+            print("error with the spell:" ,i ,"")
+            pass
     #create our json object
     jsonData = json.dumps(spellDataTab)
     # Writing JSON file
